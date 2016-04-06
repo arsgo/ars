350efd776cef385f8dac9e2292e61c16d7ef5e2e
colinyl <lib4net@126.com>
2016-04-06 22:18:26 +0800

    update

--- cluster/app.rpc.go
+++ cluster/app.rpc.go
@@ -5,11 +5,11 @@ import "fmt"
 func (a *appServer) BindRCServer(configs []*RCServerConfig, err error) error {
 	servers := make(map[string]string)
 	services := make(map[string][]string)
-	services["_"] = make([]string, 0)
+	services["-"] = make([]string, 0)
 	for _, v := range configs {
 		sv := fmt.Sprintf("%s%s", v.IP, v.Port)
 		servers[sv] = sv
-		services["_"] = append(services["_"], sv)
+		services["-"] = append(services["-"], sv)
 	}
 	a.rcServicesMap.setData(services)
 	a.rcServerPool.Register(servers)
