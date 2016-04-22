b62d397cb2bf1bdd81724490c528f03cb6896a90
lib4go@163.com <lib4go@163.com>
2016-04-22 18:17:37 +0800

    'u'

---
+++ mqservice/stomp.go
@@ -0,0 +1,47 @@
+package mqservice
+
+import (
+	"encoding/json"
+	"fmt"
+	"strings"
+
+	"github.com/colinyl/stomp"
+)
+
+type StompService struct {
+	config *StompConfig
+	broker *stomp.Stomp
+}
+
+type StompConfig struct {
+	Address string
+}
+
+func NewStompService(sconfig string) IMQService {
+	p := &StompService{}
+	err := json.Unmarshal([]byte(sconfig), &p.config)
+	if err != nil {
+		fmt.Println(err)
+		return nil
+	}
+	if strings.EqualFold(p.config.Address, "") {
+		fmt.Println("address not allowed nil")
+		return nil
+	}
+	p.broker, err = stomp.NewStomp(p.config.Address)
+	if err != nil {
+		return nil
+	}
+	return p
+}
+func (k *StompService) Send(queue string, msg string) (err error) {
+	return k.broker.Send(queue, msg)
+}
+
+func (k *StompService) Consume(queue string, callback func(stomp.MsgHandler)) (err error) {
+	return k.broker.Consume(queue,callback)
+}
+
+func (k *StompService) Close(){
+	k.broker.Close()
+}
