02c0462f1e7ec5beb543be0fbcac350ad9d5220c
lib4go@163.com <lib4go@163.com>
2016-04-22 10:01:11 +0800

    ''

--- cluster/service.provider.rpc.go
+++ cluster/service.provider.rpc.go
@@ -2,10 +2,10 @@ package cluster
 
 import "github.com/colinyl/ars/rpcservice"
 
-func (d *spServer) StartRPC() {
-	address := rpcservice.GetLocalRandomAddress()
+func (d *spServer) StartRPC()(error) {
+	address :=":2034"// rpcservice.GetLocalRandomAddress()
 	d.Port = address
 	d.dataMap.Set("port", d.Port)
 	d.rpcServer = rpcservice.NewRPCServer(address, NewScript(d))
-	d.rpcServer.Serve()
+	return d.rpcServer.Serve()
 }
