70a31d5d2768ebb2e4098cb674f297212c7ffc07
lib4go@163.com <lib4go@163.com>
2016-05-02 18:24:38 +0800

    ''

--- appserver/main.go
+++ appserver/main.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"fmt"
 	"runtime"
 
 	"github.com/colinyl/ars/cluster"
 	"github.com/colinyl/lib4go/forever"
+	"github.com/colinyl/lib4go/utility"
 )
 
 func main() {
+
+	fmt.Println(utility.GetParams("name=colin&sex=100&sex=200&order=1234567890"))
 
 	runtime.GOMAXPROCS(runtime.NumCPU())
 	appServer := cluster.NewAPPServer()
