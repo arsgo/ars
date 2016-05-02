6c83c655a3386f9a6015ad557ad908e07e679c18
colinyl <lib4net@126.com>
2016-05-02 20:49:01 +0800

    u

--- appserver/main.go
+++ appserver/main.go
@@ -2,16 +2,10 @@ package main
 
 import (
 	"runtime"
-	"sync"
 
 	"github.com/colinyl/ars/cluster"
 	"github.com/colinyl/lib4go/forever"
 )
-
-type service struct {
-	svs   map[string]string
-	mutex sync.Mutex
-}
 
 func main() {
 
