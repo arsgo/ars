b49ea2d0f3df6c714081b53fe036e3065a6174df
lib4go@163.com <lib4go@163.com>
2016-07-14 13:38:32 +0800

    'fix'

--- servers/spserver/main.go
+++ servers/spserver/main.go
@@ -8,11 +8,9 @@ import (
 )
 
 func main() {
-
-	fx, _ := os.OpenFile("C:\\tmp\\11.txt", os.O_WRONLY|os.O_CREATE|os.O_SYNC,
-		0755)
-	os.Stdout = fx
-	os.Stderr = fx
+	logFile, _ := os.OpenFile("/ext/spserver", os.O_WRONLY|os.O_CREATE|os.O_SYNC, 0755)
+	syscall.Dup2(int(logFile.Fd()), 1)
+	syscall.Dup2(int(logFile.Fd()), 2)
 
 	runtime.GOMAXPROCS(runtime.NumCPU())
 	spServer := NewSPServer()
