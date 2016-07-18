5c4a342cba893838e19b350abe3ebfb72aa4a97d
lib4go@163.com <lib4go@163.com>
2016-07-18 17:38:58 +0800

    'fix'

--- servers/rcserver/main_linux.go
+++ servers/rcserver/main_linux.go
@@ -1,23 +1,20 @@
 package main
 
 import (
-	"fmt"
-	"os"
 	"runtime"
-	"syscall"
 
 	"github.com/colinyl/lib4go/forever"
 )
 
 func main() {
 
-	logFile, err := os.OpenFile("/ext/rcserver", os.O_WRONLY|os.O_CREATE|os.O_SYNC, 0755)
+	/*logFile, err := os.OpenFile("/ext/rcserver", os.O_WRONLY|os.O_CREATE|os.O_SYNC, 0755)
 	if err != nil {
 		fmt.Println(err)
 	}
 	syscall.Dup2(int(logFile.Fd()), 1)
 	syscall.Dup2(int(logFile.Fd()), 2)
-
+	*/
 	runtime.GOMAXPROCS(runtime.NumCPU())
 	rcServer := NewRCServer()
 	f := forever.NewForever(rcServer, rcServer.Log, "rcserver", "rcserver")
