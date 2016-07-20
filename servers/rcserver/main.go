9152c556439a4fbe20354607954e26d0f6cdd600
lib4go@163.com <lib4go@163.com>
2016-07-20 09:30:57 +0800

    'fix'

--- servers/rcserver/main.go
+++ servers/rcserver/main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"fmt"
+	"os"
 	"runtime"
 
 	"github.com/colinyl/lib4go/forever"
 )
 
 func main() {
+	fp, _ := os.Getwd()
+	fmt.Println(fp)
 	runtime.GOMAXPROCS(runtime.NumCPU())
 	rcServer := NewRCServer()
 	f := forever.NewForever(rcServer, rcServer.Log, "rcserver", "rcserver")
