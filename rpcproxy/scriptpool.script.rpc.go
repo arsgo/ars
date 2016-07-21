5371fdec3f2db490aed8231f02dda009fedd5ae2
lib4go@163.com <lib4go@163.com>
2016-07-21 09:00:34 +0800

    'fix'

--- rpcproxy/scriptpool.script.rpc.go
+++ rpcproxy/scriptpool.script.rpc.go
@@ -18,6 +18,7 @@ func (s *ScriptPool) moduleRPCRequest(ls *lua.LState) int {
 	if err != nil {
 		return pushValues(ls, "", "输入参数错误")
 	}
+	s.Log.Info("module.rpc.request.session:", session)
 	r, e := s.rpcclient.Request(cmd, inputString, session)
 	if e != nil {
 		return pushValues(ls, r, e.Error())
